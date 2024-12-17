b7d93c817cf5ee7e0e04b9ebafd0853f8aab5b68
Andrey Duka <70843050+0895000@users.noreply.github.com>
2024-12-17 20:29:47 +0300

    feat: add solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -128,8 +128,11 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+
+function getAverage(arr) {
+  if (arr.length === 0) return 0;
+  const sum = arr.reduce((accum, current) => accum + current, 0);
+  return parseFloat((sum / arr.length).toFixed(2));
 }
 
 /**
@@ -142,8 +145,10 @@ function getAverage(/* arr */) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  if (arr.length === 0) return true;
+  const firstLength = arr[0].length;
+  return arr.every((element) => element.length === firstLength);
 }
 
 /**
